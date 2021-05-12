ad1593ae86ffacc20e3330a6948d4daab17aac60
Jihoon <wlgnsqkr0602@naver.com>
2021-05-12 10:55:48 +0900

    new file

--- ppo/__init__.py
+++ ppo/__init__.py
@@ -13,15 +13,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.distributions import Categorical
-import time
 
 #Hyperparameters
-learning_rate = 0.0002
-gamma         = 0.99
+learning_rate = 0.0005
+gamma         = 0.98
 lmbda         = 0.95
 eps_clip      = 0.2
 K_epoch       = 10
-T_horizon     = 128
+T_horizon     = 20
 
 class PPO(nn.Module):
     def __init__(self):
@@ -30,35 +29,36 @@ class PPO(nn.Module):
         
         self.fc1 = nn.Linear(22,64) # obs_dim = 22
         self.fc2 = nn.Linear(64,64)
+        self.fc3 = nn.Linear(64,64)
         self.fc_pi = nn.Linear(64,6) # action_dim = 6
         self.fc_v = nn.Linear(64,1)
         self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
 
     # Policy network
-    def pi(self, x):
+    def pi(self, x, softmax_dim = 0):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        
+        x = F.relu(self.fc3(x))
         x = self.fc_pi(x)
-        prob = F.tanh(x)
+        prob = F.softmax(x, dim=softmax_dim)
         return prob
     
     # Value network
     def v(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        
+        x = F.relu(self.fc3(x))
         v = self.fc_v(x)
         return v
       
     def put_data(self, transition):
         self.data.append(transition)
-
+        
     def make_batch(self):
         s_lst, a_lst, r_lst, s_prime_lst, prob_a_lst, done_lst = [], [], [], [], [], []
         for transition in self.data:
             s, a, r, s_prime, prob_a, done = transition
-
+            
             s_lst.append(s)
             a_lst.append([a])
             r_lst.append([r])
@@ -73,9 +73,6 @@ class PPO(nn.Module):
         s_prime = torch.tensor(s_prime_lst, dtype=torch.float)
         done_mask = torch.tensor(done_lst, dtype=torch.float)
         prob_a = torch.tensor(prob_a_lst)
-
-        # s = self.batch_normalization(s)
-        # s_prime = self.batch_normalization(s_prime)
 
         self.data = []
         return s, a, r, s_prime, done_mask, prob_a
@@ -104,7 +101,6 @@ class PPO(nn.Module):
             # Surrogate objective
             surr1 = ratio * advantage
             surr2 = torch.clamp(ratio, 1-eps_clip, 1+eps_clip) * advantage
-            # loss = -torch.min(surr1, surr2)
             loss = -torch.min(surr1, surr2) + F.smooth_l1_loss(self.v(s) , td_target.detach())
 
             self.optimizer.zero_grad()
@@ -113,10 +109,10 @@ class PPO(nn.Module):
         
 def main():
     # logger.set_level(logger.DISABLED)
-    env = gym.make('Walker2DBulletEnv-v0', render = False)
+    env = gym.make('Walker2DBulletEnv-v0', render = True)
     model = PPO()
     score = 0.0
-    print_interval = 100
+    print_interval = 500
 
     for n_epi in range(100000):
         s = env.reset()
@@ -126,14 +122,14 @@ def main():
                 # prob : tensor((6,), grad = softmaxbackward)
                 prob = model.pi(torch.from_numpy(s).float())
                 m = Categorical(prob)
-                '''
+                # a_max = prob.detach().numpy()
                 a_max = np.zeros(6)
                 a = m.sample().item()
                 a_max[a] = 1.0
-                '''
-                
-                s_prime, r, done, _ = env.step(a)
-                model.put_data((s, a, r, s_prime, prob.detach().numpy(), done))
+
+                s_prime, r, done, _ = env.step(a_max)
+
+                model.put_data((s, a, r, s_prime, prob[a].item(), done))
                 s = s_prime
 
                 score += r
@@ -145,6 +141,7 @@ def main():
         if n_epi % print_interval == 0 and n_epi != 0:
             print("# of episode :{}, avg score : {:.1f}".format(n_epi, score / print_interval))
             score = 0.0
+            #env.render()
 
     env.close()
 
