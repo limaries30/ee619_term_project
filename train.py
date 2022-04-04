b2200f3a8922537f7797234ff2d77bb6615b0f5e
limaries30 <limaries30@gmail.com>
2022-04-04 20:36:08 +0900

    FIX conflict

--- train.py
+++ train.py
@@ -36,7 +36,7 @@ parser.add_argument('--num_steps', type=int, default=10000001, metavar='N',
                     help='maximum number of steps (default: 1000000)')
 parser.add_argument('--hidden_size', type=int, default=512, metavar='N',
                     help='hidden size (default: 256)')
-parser.add_argument('--updates_per_step', type=int, default=2, metavar='N',
+parser.add_argument('--updates_per_step', type=int, default=4, metavar='N',
                     help='model updates per simulator step (default: 1)')
 parser.add_argument('--start_steps', type=int, default=10000, metavar='N',
                     help='Steps sampling random actions (default: 10000)')
